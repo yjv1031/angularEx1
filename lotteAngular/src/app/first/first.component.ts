113cc3da2972fdb2c5b5591532b3439c8c0c745f
yjv1031 <yjv1031@gmail.com>
2021-02-04 13:34:07 +0900

    axios commit

--- lotteAngular/src/app/first/first.component.ts
+++ lotteAngular/src/app/first/first.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-first',
@@ -7,9 +8,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FirstComponent implements OnInit {
 
+  reservationVO = {
+    carCd: 'sm01',
+    upCarCd: 'sm011',
+    carNm: '소나타'
+  };
+
+  carList = [{
+        "atchFileMbId": "",
+        "modDt": "",
+        "loginId": "",
+        "incml": "",
+        "regNm": "",
+        "arrBranchCd": null,
+        "schEmail2": "",
+        "equnr": "",
+        "pageSize": 0,
+        "pageUnit": 0,
+        "schEmail1": "",
+        "modId": "",
+        "fuelcdTx": "",
+        "jsFunction": null,
+        "carNo": "",
+        "joindt": "",
+        "viewCnt": "",
+        "carcdTx": "",
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  carNo(car) {
+    alert(car.carNo);
+  }
+
+  callCarList() {
+    axios({
+      method: 'post',
+      url: 'http://t.lotterentacar.net/hp/kor/funcCommon/usedCarTheme1Info.json',
+      data: {}
+    }).then(
+      (res) => {
+        console.log(JSON.stringify(res, null, 2));
+        this.carList = res.data.resultList;
+      }
+    );
+  }
 }
