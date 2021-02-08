575d7b4420b0cc485e65523d2a0ebcc1ebdfcd9c
yjv1031 <yjv1031@gmail.com>
2021-02-08 16:08:06 +0900

    crud update

--- lotteAngular/src/app/first/first.component.ts
+++ lotteAngular/src/app/first/first.component.ts
@@ -14,25 +14,21 @@ export class FirstComponent implements OnInit {
     carNm: '소나타'
   };
 
-  carList = [
-    {
-      title: '소나타111'
-      ,content: 'asdff'      
-    }
+  noticeList = [
   ];
+
+  emptyNotice = {
+    title:'',
+    content:'',
+    writer:'',
+  }
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  carNo(car) {
-    alert(car.title);
-    car.title += 'zzzz';
-  }
-
-  callCarList() {
-    this.reservationVO.carNm = '111';
+  callNoticeList() {
     axios({
       method: 'get',
       url: '/lot/notice/list',
@@ -40,7 +36,8 @@ export class FirstComponent implements OnInit {
     }).then(
       (res) => {
         console.log(JSON.stringify(res, null, 2));
-        this.carList = res.data.resultData;
+        const data = res.data;
+        this.noticeList = data.resultData;
       }
     );
   }
@@ -54,7 +51,59 @@ export class FirstComponent implements OnInit {
     }).then(
       (res) => {
         console.log(JSON.stringify(res, null, 2));
-        this.carList = res.data.resultData;
+
+        const data = res.data;
+        if(data.resultCode > 0) {
+          alert('정상적으로 업데이트 되었습니다');
+        } else {
+          alert('업데이트를 실패하였습니다');
+        }
+
+        this.noticeList = data.resultData;
+      }
+    );
+  }
+
+  insertNotice() {
+    console.log(JSON.stringify(this.emptyNotice, null, 2));
+    axios({
+      method: 'post',
+      url: '/lot/notice',
+      data: this.emptyNotice
+    }).then(
+      (res) => {
+        console.log(JSON.stringify(res, null, 2));
+
+        const data = res.data;
+        if(data.resultCode > 0) {
+          alert('정상적으로 작성 되었습니다');
+        } else {
+          alert('작성을 실패하였습니다');
+        }
+
+        this.noticeList = data.resultData;
+      }
+    );
+  }
+
+  deleteNotice(car) {
+    console.log(JSON.stringify(car, null, 2));
+    axios({
+      method: 'delete',
+      url: '/lot/notice',
+      data: car
+    }).then(
+      (res) => {
+        console.log(JSON.stringify(res, null, 2));
+
+        const data = res.data;
+        if(data.resultCode > 0) {
+          alert('정상적으로 삭제 되었습니다');
+        } else {
+          alert('삭제를 실패하였습니다');
+        }
+
+        this.noticeList = data.resultData;
       }
     );
   }
