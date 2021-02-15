09f7d26fc5f47ac0cf15a1b40c6f4dbb2e672f1c
choi <uyacos@gmail.com>
2021-02-15 17:07:25 +0900

    test2

--- lotteAngular/src/app/second/second.component.ts
+++ lotteAngular/src/app/second/second.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-second',
@@ -10,6 +11,50 @@ export class SecondComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.callCarLifePostList();
+  }
+
+  isContsDetail = false;
+
+  postList = [
+  ];
+
+  callCarLifePostList() {
+    axios({
+      method: 'get',
+      url: '/lot/carLifePost/list',
+      data: {}
+    }).then(
+      (res) => {
+        console.log(JSON.stringify(res, null, 2));
+        const data = res.data;
+        this.postList = data.resultData;
+        
+      }
+    );
+  }
+
+  postDetail = {
+    postSeq:'',
+    postTitl1:'',
+    postTitl2:'',
+    postStDt:'',
+    postEndDt:'',
+  }
+
+  getPostDetail(post) {
+    console.log(post);
+    axios({
+      method: 'get',
+      url: '/lot/carLifePost',
+      data: {"postSeq" : post.postSeq}
+    }).then(
+      (res) => {
+        console.log(JSON.stringify(res, null, 2));
+        this.postDetail = res.data.resultData;
+        this.isContsDetail = true;
+      }
+    );
   }
 
 }
